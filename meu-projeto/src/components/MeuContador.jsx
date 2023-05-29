9b4f6424f553f44a75b43605908e502865e390bd
zauzaud <gabriel.zaude@gmail.com>
2023-05-29 02:51:37 -0300

    Update MeuContador.jsx

--- meu-projeto/src/components/MeuContador.jsx
+++ meu-projeto/src/components/MeuContador.jsx
@@ -6,12 +6,29 @@ export default function MeuContador() {
   function aumentar() {
     setContador(contador + 1);
   }
+  function diminuir() {
+    setContador(contador - 1);
+  }
+
+  if (contador > 5) {
+    return (
+      <div>
+        <h1>Valor muito alto</h1>
+        <button onClick={aumentar}>Aumentar</button>
+        <button onClick={diminuir}>Diminuir</button>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1>Meu Contador</h1>
       <h3>{contador}</h3>
       <button onClick={aumentar}>Aumentar</button>
+      <button onClick={diminuir}>Diminuir</button>
+      {contador > 9 ? <h3>Contador maior que 9</h3> : null}{" "}
+      {/* if ternário, se
+      contador for maior que 9, exibe o texto, caso contrário, não exibe nada */}
     </div>
   );
 }
